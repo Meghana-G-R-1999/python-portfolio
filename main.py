191370629c9a0ef266953a1b737efb6080ece9b7
Meghana-G-R-1999 <meghanagr999@gmail.com>
2024-07-19 18:57:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+
+print("Welcome to Generator your own brand name program.")
+
+city = input("Please enter the city you grew up: ")
+character = input("Please enter your favorite cartoon character: ")
+
+print(f"your name is: {city + character}.")
